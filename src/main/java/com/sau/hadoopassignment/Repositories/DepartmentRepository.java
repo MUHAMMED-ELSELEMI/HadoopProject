fd1a7cc3923a592766ece01a7caf57e6ff347d02
muhammed <muhammed.elselemi@ogr.sakarya.edu.tr>
2024-11-06 16:08:28 +0300

    modification on Repositories

--- src/main/java/com/sau/hadoopassignment/Repositories/DepartmentRepository.java
+++ src/main/java/com/sau/hadoopassignment/Repositories/DepartmentRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DepartmentRepository extends JpaRepository<Departments, Integer> {
+
+    Departments findByDname(String dname); // Custom method to find department by name
+
 }
